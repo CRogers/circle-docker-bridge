cc6357361f38c967723b5474e46e149eac25ae2c
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-01 17:50:20 +0100

    Give a good attempt at AOT compiling the docker-spotify lib

--- src/main/java/uk/callumr/circledockerbridge/Main.java
+++ src/main/java/uk/callumr/circledockerbridge/Main.java
@@ -1,8 +1,21 @@
 package uk.callumr.circledockerbridge;
 
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.DockerClient;
+import com.spotify.docker.client.exceptions.DockerCertificateException;
+import com.spotify.docker.client.exceptions.DockerException;
+import com.spotify.docker.client.messages.Container;
+
+import java.util.List;
+
 public class Main {
-    public static void main(String... args) {
+    public static void main(String... args) throws DockerCertificateException, DockerException, InterruptedException {
         System.out.println("hi");
+
+        DockerClient dockerClient = DefaultDockerClient.fromEnv().build();
+
+        List<Container> containers = dockerClient.listContainers();
+        System.out.println("containers = " + containers);
     }
 }
 
