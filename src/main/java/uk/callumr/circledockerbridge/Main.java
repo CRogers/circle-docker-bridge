958e9211d8a372bb088cfd1b0e1d2b988ada6ff0
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-02 15:35:51 +0100

    Remove massive dependency that actually makes things harder

--- src/main/java/uk/callumr/circledockerbridge/Main.java
+++ src/main/java/uk/callumr/circledockerbridge/Main.java
@@ -1,21 +1,8 @@
 package uk.callumr.circledockerbridge;
 
-import com.spotify.docker.client.DefaultDockerClient;
-import com.spotify.docker.client.DockerClient;
-import com.spotify.docker.client.exceptions.DockerCertificateException;
-import com.spotify.docker.client.exceptions.DockerException;
-import com.spotify.docker.client.messages.Container;
-
-import java.util.List;
-
 public class Main {
-    public static void main(String... args) throws DockerCertificateException, DockerException, InterruptedException {
+    public static void main(String... args) {
         System.out.println("hi");
-
-        DockerClient dockerClient = DefaultDockerClient.fromEnv().build();
-
-        List<Container> containers = dockerClient.listContainers();
-        System.out.println("containers = " + containers);
     }
 }
 
