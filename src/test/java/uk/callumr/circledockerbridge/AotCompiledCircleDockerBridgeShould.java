611c7a436a086ccc96312a01a354d2ab2121b71d
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-04 16:20:05 +0100

    Test AoT compile one too

---
+++ src/test/java/uk/callumr/circledockerbridge/AotCompiledCircleDockerBridgeShould.java
@@ -0,0 +1,31 @@
+package uk.callumr.circledockerbridge;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zeroturnaround.exec.ProcessExecutor;
+import org.zeroturnaround.exec.stream.slf4j.Slf4jInfoOutputStream;
+
+import java.io.IOException;
+
+public class AotCompiledCircleDockerBridgeShould extends CircleDockerBridgeShould {
+    private final Logger log = LoggerFactory.getLogger(AotCompiledCircleDockerBridgeShould.class);
+
+    @Override
+    protected void startBridge() {
+        try {
+            new ProcessExecutor()
+                    .command("build/native/circle-docker-brige")
+                    .exitValue(0)
+                    .redirectOutput(new Slf4jInfoOutputStream(log))
+                    .redirectError(new Slf4jInfoOutputStream(log))
+                    .start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected int portMadeFor(int port) {
+        return port;
+    }
+}
