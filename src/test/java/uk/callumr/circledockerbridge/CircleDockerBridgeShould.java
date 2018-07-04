3c2352f5aa2dc607ab97a52fdc1ba213b0d90314
Callum Rogers <callumrogers@hotmail.co.uk>
2018-07-04 17:22:36 +0100

    Awaitility for less fagile tests

--- src/test/java/uk/callumr/circledockerbridge/CircleDockerBridgeShould.java
+++ src/test/java/uk/callumr/circledockerbridge/CircleDockerBridgeShould.java
@@ -2,6 +2,7 @@ package uk.callumr.circledockerbridge;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
+import org.awaitility.Duration;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -9,11 +10,12 @@ import org.slf4j.LoggerFactory;
 import uk.callumr.circledockerbridge.docker.ContainerId;
 import uk.callumr.circledockerbridge.docker.Docker;
 
-import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.equalTo;
 import static uk.callumr.circledockerbridge.DockerImages.SKYSCANNER_HTTPBIN;
 
 public abstract class CircleDockerBridgeShould {
@@ -43,21 +45,17 @@ public abstract class CircleDockerBridgeShould {
 
         System.out.println("mappedPort = " + mappedPort);
 
-        DockerTestUtils.definitelyKillContainerAfter(containerId, () -> {
-            try {
-                Thread.sleep(1000);
-
-                HttpURLConnection httpGet = (HttpURLConnection) new URL(String.format("http://localhost:%d/get?yolo=hi", mappedPort)).openConnection();
-                String response = IOUtils.toString(httpGet.getInputStream());
-                String yoloValue = new ObjectMapper().readTree(response)
-                        .get("args")
-                        .get("yolo")
-                        .asText();
-
-                assertThat(yoloValue).isEqualTo("hi");
-            } catch (InterruptedException | IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        DockerTestUtils.definitelyKillContainerAfter(containerId, () -> await()
+                .atMost(10, TimeUnit.SECONDS)
+                .pollInterval(Duration.ONE_HUNDRED_MILLISECONDS)
+                .ignoreExceptions()
+                .until(() -> {
+                    HttpURLConnection httpGet = (HttpURLConnection) new URL(String.format("http://localhost:%d/get?yolo=hi", mappedPort)).openConnection();
+                    String response = IOUtils.toString(httpGet.getInputStream());
+                    return new ObjectMapper().readTree(response)
+                            .get("args")
+                            .get("yolo")
+                            .asText();
+                }, equalTo("hi")));
     }
 }
